92be5842d0865cecd53537005496dd717a187b8d
ritiktyagi1 <ritiktyagi110@gmail.com>
2022-07-24 23:45:57 +0530

    number system codes

---
+++ Pepcoding/Function and array/Any_Base_To_Decimal.java
@@ -0,0 +1,24 @@
+import java.util.*;
+  
+  public class Main{
+  
+  public static void main(String[] args) {
+      Scanner scn = new Scanner(System.in);
+      int n = scn.nextInt();
+      int b = scn.nextInt();
+      int d = getValueIndecimal(n, b);
+      System.out.println(d);
+   }
+  
+   public static int getValueIndecimal(int n, int b){
+      // write your code here
+      int res=0,mul=0;
+      while(n>0){
+          int rem=n%10;
+          n=n/10;
+          res+=rem*Math.pow(b,mul);
+          mul++;
+      }
+      return res;
+   }
+  }
